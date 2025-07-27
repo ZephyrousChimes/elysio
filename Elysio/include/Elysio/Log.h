cce5b8fd0f954939bed4025aa3720a6f11b7688f
KushalSuvan <kushalsuvanjenamani@gmail.com>
2025-07-27 19:58:44 +0530

    feat: added clang formatting

---
+++ Elysio/include/Elysio/Log.h
@@ -0,0 +1,14 @@
+//
+// Created by kronos on 7/27/25.
+//
+
+#pragma once
+
+#include "Core.h"
+
+class Log {
+public:
+    Log();
+    ~Log();
+};
+
---
+++ Elysio/src/Log.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kronos on 7/27/25.
+//
+
+#include "Log.h"
